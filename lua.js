d469592aeeea3065dae730ef789b77272fc20d0f
niko <68440743+Nikkwo@users.noreply.github.com>
2023-05-15 02:45:33 +0000

    Update lua.js

--- lua.js
+++ lua.js
@@ -3,7 +3,7 @@
 
 // Quando você precisar mostrar o diálogo:
 //dialogText.innerHTML = "Este é o texto do diálogo.";
-//dialogBox.style.display = "block";
+//dialogBox.style.display = "block"; //bloqueia o texto na tela
 
 //botoes
 //const (var nome) = document.getElementById("botao1");
@@ -17,7 +17,17 @@ const dialogText = document.getElementById("dialog-text");
 dialogText.innerHTML = "Você está... sozinho?"
 dialogBox.style.display = "Block";
 
-const container = document.getElementById("botoes-container");
-const botaoSim = document.createElement("button");
-botaoSim.innerHTML = "Sim";
-container.appendChild(botaoSim);
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", continuarHistoria);
+btn1.addEventListener("click", continuarHistoria);
+
+function continuarHistoria() {
+    if (this.id === "btn1") {
+      dialogText.innerHTML = "a lua pálida sorri para você";
+      dialogBox.style.display = "block";
+    } else if (this.id === "btn2") {
+      dialogText.innerHTML = "Nunca estamos sozinhos...";
+      dialogBox.style.display = "block";
+    }
+  }

edbddb7af8fa6907df29e4ee09036b39e4b3a1d2
niko <68440743+Nikkwo@users.noreply.github.com>
2023-05-15 01:20:35 +0000

    Update lua.js

--- lua.js
+++ lua.js
@@ -1,61 +1,23 @@
-$(document).ready(function() {
-  // Defina as opções de texto
-  var options = [
-    {
-      text: "Sim!",
-      nextText: "Você morreu, nunca estamos sozinhos."
-    },
-    {
-      text: "Não...",
-      nextText: "Você passou do nível 1..."
-    }
-  ];
-  
-  // Defina a função para atualizar o texto do jogo
-  function updateGameText(text) {
-    $('#game-text').text(text);
-  }
+// Quando o diálogo terminar:
+//dialogBox.style.display = "none";
 
-  // Defina a função para adicionar as opções do jogo
-  function addGameOptions(options) {
-    $('#options-container').empty();
-    options.forEach(function(option) {
-      var $option = $('<button class="game-option"></button>');
-      $option.text(option.text);
-      $option.click(function() {
-        if (option.text === "Sim!") {
-          // Desativar os botões e exibir mensagem de fim de jogo
-          $('.game-option').off('click');
-          updateGameText("Você morreu. Fim de jogo!");
-        } else {
-          // Atualizar o texto do jogo e adicionar as opções para a próxima cena
-          setTimeout(function() {
-            updateGameText(option.nextText);
-            addGameOptions([]);
-          }, 1000);
-        }
-      });
-      $('#options-container').append($option);
-    });
-  }
-  
-  // Inicialize o jogo com o texto e opções padrão
-  updateGameText('');
-  addGameOptions(options);
-  
-  // Adicione o efeito de texto digitado
-  const texto = "Você está sozinho?";
-  let i = 0;
-  const tempoDigitacao = 80; // em milissegundos
-  
-  function escreverTexto() {
-    if (i < texto.length) {
-      $('#game-text').text($('#game-text').text() + texto.charAt(i));
-      i++;
-      setTimeout(escreverTexto, tempoDigitacao);
-    }
-  }
-  
-  escreverTexto();
-  
-});
+// Quando você precisar mostrar o diálogo:
+//dialogText.innerHTML = "Este é o texto do diálogo.";
+//dialogBox.style.display = "block";
+
+//botoes
+//const (var nome) = document.getElementById("botao1");
+//(var nome).style.display = "none"; // oculta o botão
+//(var nome).style.display = "block"; // exibe o botão
+
+
+
+const dialogBox = document.getElementById("dialog-box");
+const dialogText = document.getElementById("dialog-text");
+dialogText.innerHTML = "Você está... sozinho?"
+dialogBox.style.display = "Block";
+
+const container = document.getElementById("botoes-container");
+const botaoSim = document.createElement("button");
+botaoSim.innerHTML = "Sim";
+container.appendChild(botaoSim);

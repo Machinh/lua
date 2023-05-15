74e65ee99dbced65e070f7347bcd3b50c0c610ae
niko <68440743+Nikkwo@users.noreply.github.com>
2023-05-15 16:58:52 +0000

    Update lua.js

--- lua.js
+++ lua.js
@@ -10,6 +10,8 @@
 //(var nome).style.display = "none"; // oculta o botão
 //(var nome).style.display = "block"; // exibe o botão
 
+//função
+//podeContinuar = false; //deixa os botões inutilizaveis
 
 
 const dialogBox = document.getElementById("dialog-box");
@@ -22,12 +24,19 @@ const btn2 = document.getElementById("btn2");
 btn2.addEventListener("click", continuarHistoria);
 btn1.addEventListener("click", continuarHistoria);
 
+
+
+
+let podeContinuar = true;
+
 function continuarHistoria() {
-    if (this.id === "btn1") {
-      dialogText.innerHTML = "a lua pálida sorri para você";
-      dialogBox.style.display = "block";
-    } else if (this.id === "btn2") {
-      dialogText.innerHTML = "Nunca estamos sozinhos...";
-      dialogBox.style.display = "block";
-    }
+  if (this.id === "btn1" && podeContinuar) {
+    dialogText.innerHTML = "a lua pálida sorri para você";
+    dialogBox.style.display = "block";
+    podeContinuar = true; // nenhum outro botão pode ser clicado
+  } else if (this.id === "btn2" && podeContinuar) {
+    dialogText.innerHTML = "Resposta errada, nunca estamos sozinhos...";
+    dialogBox.style.display = "block";
+    podeContinuar = false; // nenhum outro botão pode ser clicado
   }
+}
